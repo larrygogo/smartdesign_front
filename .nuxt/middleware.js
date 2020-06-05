269757381cbbd335207a82b569629a6bdfc6bcfc
LarryZheng <351220018@qq.com>
2020-06-05 09:57:49 +0800

    移除编译文件

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../client/middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['verify'] = require('../client/middleware/verify.js')
-middleware['verify'] = middleware['verify'].default || middleware['verify']
-
-export default middleware
