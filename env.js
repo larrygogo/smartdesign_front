d9f1c9e4ee7edae9dab2d8e9c5d526b4bdd9a2fb
LarryZheng <351220018@qq.com>
2020-06-05 09:58:42 +0800

    修改api

--- env.js
+++ env.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   production: {
     NODE_ENV: "production",
-    ENV_API: "http://api.toolbee.cn",
+    ENV_API: "http://api.toolbee.larryzheng.cn",
     ROUTER_BASE: "/"
   }
 }
