49cae108c64d3505b8b72559df4cd120ff22f582
larrygogo <351220018@qq.com>
2020-06-06 16:20:23 +0800

    更改baseurl

--- env.js
+++ env.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   production: {
     NODE_ENV: "production",
-    ENV_API: "http://api.toolbee.larryzheng.cn",
+    ENV_API: "http://api.toolbee.cn",
     ROUTER_BASE: "/"
   }
 }
