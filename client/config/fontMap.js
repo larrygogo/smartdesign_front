4397ef5486d2c73c9b3c6459fc425e87126abe30
larrygogo <351220018@qq.com>
2021-03-09 16:35:14 +0800

    新增字体

--- client/config/fontMap.js
+++ client/config/fontMap.js
@@ -1,14 +1,32 @@
 export default [
-    {
-        id: "zh3",
-        name: "字魂3号-英雄黑体",
-        fontName: "zihun3hao\\-yingxiongheiti",
-        image: "/images/fonts/zh3.svg",
-    },
-    {
-        id: "zh78",
-        name: "字魂78号-空灵体",
-        fontName: "zihun78hao\\-konglignti",
-        image: "/images/fonts/zh78.svg",
-    }
-]
+  {
+    id: "zh3",
+    name: "字魂3号-英雄黑体",
+    fontName: "zihun3hao\\-yingxiongheiti",
+    image: "/images/fonts/zh3.svg",
+  },
+  {
+    id: "zh27",
+    name: "字魂27号-布丁体",
+    fontName: "zihun27hao\\-budingti",
+    image: "/images/fonts/zh27.svg",
+  },
+  {
+    id: "zh45",
+    name: "字魂45号-冰宇雅宋",
+    fontName: "zihun45hao\\-bingyuyasong",
+    image: "/images/fonts/zh45.svg",
+  },
+  {
+    id: "zh59",
+    name: "字魂59号-创粗黑",
+    fontName: "zihun59hao\\-chuangcuhei",
+    image: "/images/fonts/zh59.svg",
+  },
+  {
+    id: "zh78",
+    name: "字魂78号-空灵体",
+    fontName: "zihun78hao\\-konglignti",
+    image: "/images/fonts/zh78.svg",
+  },
+];
